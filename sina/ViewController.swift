9285ecd838d72d890c9c46848692b7085d146c75
Siavash Mosadegh <s.mosadegh@rocketmail.com>
2020-05-28 22:32:16 +0430

    Initial Commit

---
+++ sina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sina
+//
+//  Created by Siavash Mosadegh on 5/28/20.
+//  Copyright © 2020 Siavash Mosadegh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
